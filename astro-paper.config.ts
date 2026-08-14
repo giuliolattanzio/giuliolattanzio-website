a363767bd47dbfba31bdc4c4654f6e2dd3c202ad
giuliolattanzio <giulio.lattanzio@gmail.com>
2026-08-14 12:05:35 +0200

    Update astro-paper.config.ts

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -24,7 +24,6 @@ export default defineAstroPaperConfig({
     showBackButton: true,
     editPost: {
       enabled: false,
-      url: "https://github.com/giuliolattanzio/giuliolattanzio-website/edit/main/",
     },
     search: "pagefind",
   },
